c5b37671481d54bbd3a8449a0ae2dd39acb6a632
Griffin Heyrich <gheyrich@csa1.bu.edu>
2020-09-08 13:25:40 -0400

    added switch statement to deal with each month

--- assignment-1.c
+++ assignment-1.c
@@ -9,6 +9,48 @@ int main() {
     int month, day;
 
     printf("Enter date (mm/dd): ");
+    scanf("%d/%d", &month, &day); //scan for month and day
+        if ((day > 31 || day < 1) || (month > 12 || month < 1) ){ //check to see if valid 
+            return 1; 
+        }
+        if (month == 2 && day > 28) { //is valid in feb
+            return 1; 
+        }
+        switch(month){ //switch statement to send to certain month
+            case 1 : printf("No class");
+            break;
+            case 2 : printf("No class");
+            break;
+            case 3 : printf("No class");
+            break;
+            case 4 : printf("No class");
+            break;
+            case 5 : printf("No class");
+            break;
+            case 6 : printf("No class");
+            break;
+            case 7 : printf("No class");
+            break;
+            case 8 : printf("No class");
+            break;
+            case 9 : if(day == 3 || day == 8 || day == 10 || day == 15 || day == 17 || day == 22 || day == 24 || day == 29){
+                    printf("Lecture.");
+                }
+                if (day == 14 || day == 21 || day ==28){
+                    printf("Discussion");
+                }
+                if (day == 7){
+                    printf("Labor Day.");
+                }
+            break;
+            case 10 : 
+            break;
+            case 11 : 
+            break;
+            case 12: 
+            break;
+        }
+
     // ADD Your code.  Be sure your output matches exactly.  
     // Check using reference binary
     // new comment
